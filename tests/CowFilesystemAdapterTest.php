4bc3c17f406f7ae534ac7a7c1462295c74b390cd
Antonio J. García Lagar <aj@garcialagar.es>
2025-05-30 09:02:14 +0200

    Update CS config

--- tests/CowFilesystemAdapterTest.php
+++ tests/CowFilesystemAdapterTest.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * AJGL Flysystem COW adapter
+ *
+ * Copyright (C) Antonio J. García Lagar <aj@garcialagar.es>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Ajgl\Flysystem\Cow\Tests;
 
 use Ajgl\Flysystem\Cow\CowFilesystemAdapter;
